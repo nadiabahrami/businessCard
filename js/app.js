4c21d2b2336a77273ac79f47e53161bd61cdf4e5
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-01-04 19:17:21 -0800

    initial objects created and inital css style sheet

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var jobs = [
+  {
+    title: 'MLBucket list',
+    collaborators: 'Paul Sheridan, Kate Zurlo_Cuva, Shelly Y',
+    projectURL: 'http://nadiabahrami.github.io/bucketlist',
+    releaseDate: '12-29-15',
+    description: 'MLBucketlist is a web app that allows the user to route a mpa to any baseball stadiums that they would like to bisit on a road trip.  It will produce a route and directions to each selected stadium.  It also allows storage of comments for each visit and store that information to local storage to act as a scrap book.'
+  },
+  {
+    title: 'Salmon Cookie Shop ',
+    collaborators: 'none',
+    projectURL: 'http://nadiabahrami.github.io/cookie-stand',
+    releaseDate: '12-13-15',
+    description: 'This is an example of a websit for a buisness that is selling salmon based cookies.   It has an tool rendering sales numbers that is not attached to the public facing sight.   Please see the rep for that site under the folder "internal-docs".'
+  }
+];
