a0f53f6dac6750983c78ff069a799b24646e0b92
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-01-07 22:14:58 -0800

    inital color trial

--- js/projects.js
+++ js/projects.js
@@ -16,38 +16,3 @@ var projects = [
     description: 'This is an example of a website for a buisness that is selling salmon based cookies.   It has an tool rendering sales numbers that is not attached to the public facing sight.   Please see the repo for that site under the folder "internal_docs".  This was an inital go at dyanmically creating html objects using javascript.   This was an intial attempt at css'
   }
 ];
-
-var articles = [];
-
-function Article (opts){
-  this.title = opts.title;
-  this.author = opts.author;
-  this.collaborators = opts.collaborators;
-  this.projectURL = opts.projectURL;
-  this.releaseDate = opts.releaseDate;
-  this.description = opts.description;
-};
-
-Article.prototype.toHtml = function() {
-  var $newArticle = $('article.template').clone();
-  $newArticle.find('#title').html(this.title);
-  $newArticle.find('.date').html(this.releaseDate);
-  $newArticle.find('.colab').html('<strong>Collaborators: </strong>'+this.collaborators);
-  $newArticle.find('.url').html('<strong>Project URl: </strong>'+this.projectURL);
-  $newArticle.find('.description').html('<strong>Project Description:</strong>'+this.description);
-
-
-
-
-  $newArticle.append('<hr>');
-  $newArticle.removeClass('template');
-  return $newArticle;
-};
-
-projects.forEach(function(ele) {
-  articles.push(new Article(ele));
-});
-
-articles.forEach(function(a){
-  $('#articles').append(a.toHtml());
-});
