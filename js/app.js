72bf1a6605a2873f0483642014b82bf61eca0446
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-01-12 09:22:52 -0800

    Ajax call is working for the articlearticle.  Woot!  Also checks to see if ETAG is in local storage

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-var articles = [];
-
 function Article (opts){
   this.title = opts.title;
   this.author = opts.author;
@@ -9,18 +7,55 @@ function Article (opts){
   this.description = opts.description;
 };
 
+Article.all= [];
+
 Article.prototype.toHtml = function() {
+
   var appTemplate = $('#ourBuild').html();
   var compiledTemplate = Handlebars.compile(appTemplate);
   var html = compiledTemplate(this);
   $('#articles').append(html);
-
 };
 
-projects.forEach(function(ele) {
-  articles.push(new Article(ele));
-});
+Article.loadAll = function(projects) {
+  projects.forEach(function(ele) {
+    Article.all.push(new Article(ele));
+  });
+};
 
-articles.forEach(function(a){
-  $('#articles').append(a.toHtml());
-});
+Article.serverGrab = function (){
+  $.ajax({
+    url: '/data/projects.json',
+    Type: 'GET',
+    dataType: 'json',
+    success: function(rawData, message, xhr){
+      Article.loadAll(rawData);
+      localStorage.rawData = JSON.stringify(rawData);
+      articleView.initIndexPage();
+      localStorage.savedETag = JSON.stringify(xhr.getResponseHeader('ETag'));
+    },
+    error: function(){
+      console.log('fuck');
+    }
+  });
+};
+
+Article.fetchAll = function() {
+  if (localStorage.rawData) {
+    $.ajax({
+      url: '/data/projects.json',
+      Type: 'HEAD',
+      success: function(data, message, xhr) {
+        var getETag = xhr.getResponseHeader('ETag');
+        if(getETag === JSON.parse(localStorage.savedETag)) {
+          Article.loadAll(JSON.parse(localStorage.rawData));
+          articleView.initIndexPage();
+        }else{
+          Article.serverGrab();
+        }
+      }
+    });
+  }else{
+    Article.serverGrab();
+  };
+};
